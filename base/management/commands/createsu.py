eb59b6a01dbd9e8c594a5d86cfc3839b8d2b3026
sujith <sujithforcoding@gmail.com>
2023-03-18 22:29:25 +0530

    ooi

--- base/management/commands/createsu.py
+++ base/management/commands/createsu.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='sujith').exists():
             User.objects.create_superuser(
-                username='admin',
-                password='complexpassword123'
+                username='sujith',
+                password='sujith@123'
             )
         print('Superuser has been created.')
