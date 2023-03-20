f2459e154d4ec7152d6fa7cdd311ce1ab5d4388e
sujith <sujithforcoding@gmail.com>
2023-03-21 01:19:05 +0530

    unnecessary url id removed

--- base/urls.py
+++ base/urls.py
@@ -6,9 +6,9 @@ from .views import MyTokenObtainPairView, registerScraper, scraperAdminProfileCr
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/scraper/', registerScraper, name='registerScraper'),
-    path('scraper/admin/profile-creator/<int:id>/', scraperAdminProfileCreator, name='scraperAdminProfileCreator'),
-    path('scraper/admin/all-staffs/<int:id>/', getAllStaffs, name='getAllStaffs'),
+    path('scraper/admin/profile-creator/', scraperAdminProfileCreator, name='scraperAdminProfileCreator'),
+    path('scraper/admin/all-staffs/', getAllStaffs, name='getAllStaffs'),
     path('scraper/admin/deactivate-staff/<int:id>/', deactivateStaff, name='deactivateStaff'),
-    path('scraper/staff/add-staff/<int:id>/', addStaffToBusiness, name='addStaffToBusiness'),
+    path('scraper/staff/add-staff/', addStaffToBusiness, name='addStaffToBusiness'),
 
 ]
