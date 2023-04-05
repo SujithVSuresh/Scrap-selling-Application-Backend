c346c8031d1d94794e94fdbf7f205fddfb18dfe0
sujith <sujithforcoding@gmail.com>
2023-04-06 00:27:05 +0530

    sdfdf

--- base/urls.py
+++ base/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('user/create-pickup-address/', createPickupAddresses, name='createPickupAddresses'),
     path('user/sellrequest-orders/', getAllSellRequestOrders, name='getAllSellRequestOrders'),
     path('user/sellrequest-orders/manage-review/<int:id>/', manageOrderReview, name='createOrderReview'),
-    path('user/sellrequest-order/cancel/', cancelSellRequest, name='cancelSellRequest'),
+    path('user/sellrequest-order/cancel/<int:id>/', cancelSellRequest, name='cancelSellRequest'),
 
 
 ]
