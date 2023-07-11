8af11948a04fd22bcc9dc9e9cb70090d5eb741a3
SujithVSuresh <sujitheternal13@gmail.com>
2023-07-11 20:03:59 +0530

    second db built commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -88,16 +85,16 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 #}
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
 
 
 # Password validation
--- base/management/commands/createsu.py
+++ base/management/commands/createsu.py
@@ -10,9 +10,9 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='ajith').exists():
+        if not User.objects.filter(username='user').exists():
             User.objects.create_superuser(
-                username='ajith',
-                password='ajith@123'
+                username='user',
+                password='pwd@123'
             )
         print('Superuser has been created.')
